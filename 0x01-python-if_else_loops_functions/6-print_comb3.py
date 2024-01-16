db10c284a53f3004593b683c731062df0fcba2eb
Mohamed Super Dumbuya <dumbuya366@gmail.com>
2024-01-16 23:09:00 +0100

    print num

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Godswill Kalu
+
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+        else:
+            print("{}{}".format(num1, num2), end=", ")
