fc39721bc16e97a10c7cfa751b0c6ce5d91d2c82
Mohamed Super Dumbuya <dumbuya366@gmail.com>
2024-01-16 23:38:50 +0100

    print combo

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -3,4 +3,3 @@ uppercase = __import__('8-uppercase').uppercase
 
 uppercase("best")
 uppercase("Best School 98 Battery street")
-
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
+#!/usr/bin/python3
+# Author - Godswill Kalu
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
