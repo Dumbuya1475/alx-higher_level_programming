cd6246c7c2745d3a232e260d596a83b3191b1b4e
Mohamed Super Dumbuya <dumbuya366@gmail.com>
2024-01-18 22:46:02 +0100

    Resolve conflicts and add untracked files

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -9,6 +9,10 @@ def magic_calculation(a, b):
             c = add(c, i)
         return (c)
 
+<<<<<<< Updated upstream
     else:
         return(sub(a, b))
 
+=======
+    return(sub(a, b))
+>>>>>>> Stashed changes
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from variable_load_5 import a
+    from variable_load_5 import 
 
     print("{}".format(a))
